e80b8418798c433b74f30d66037342c121d6f74f
Fethi Isfarca <fisfarca@googlemail.com>
2018-08-12 20:34:00 +0200

    Mutation icons

---
+++ Darwin/Assets/Scripts/MutationHandler/MutationSprites.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MutationSprites : MonoBehaviour
+{
+    // Declare variables.
+    public GameObject[] Images;
+    public Sprite[] Selected;
+    public Sprite[] DeSelected;
+    
+    // 1. MutationIcons serializefield entfernen. ERLEDIGT
+    // 2. MutationIcons with Update for delection/selection icons.
+    // 3. Transformation script for player.
+}
