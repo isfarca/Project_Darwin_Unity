12a80899b043e90629e0ac12a2bd7516a5c67f3d
Fethi Isfarca <fisfarca@googlemail.com>
2018-08-14 21:16:19 +0200

    Mutation handling

---
+++ Darwin/Assets/Scripts/HUD/MutationGameplayButton.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MutationGameplayButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    // Declare variables.
+    [SerializeField] private MutationHandler.MutationType _mutationType;
+    private MutationHandler _mutationSpritesScript;
+    private int _elephantCounter, _fishCounter, _hawkCounter, _rhinoCounter, _turtleCounter;
+
+    /// <summary>
+    /// Calling before start.
+    /// </summary>
+    private void Awake()
+    {
+        // Get the sprites.
+        _mutationSpritesScript = GameObject.FindGameObjectWithTag("MutationHandler").GetComponent<MutationHandler>();
+    }
+
+    /// <summary>
+    /// Calling by start.
+    /// </summary>
+    private void Start()
+    {
+        // Set default values.
+        _elephantCounter = _fishCounter = _hawkCounter = _rhinoCounter = _turtleCounter = 0;
+    }
+
+    // Calling by clicking or touching.
+    public void OnPointerDown(PointerEventData eventData) 
+    {
+        switch (_mutationType)
+        {
+            case MutationHandler.MutationType.Elephant:
+                
+                // Who are you?
+                _mutationSpritesScript.IsNormal = false;
+                _mutationSpritesScript.IsElephant = true;
+                _mutationSpritesScript.IsFish = false;
+                _mutationSpritesScript.IsHawk = false;
+                _mutationSpritesScript.IsRhino = false;
+                _mutationSpritesScript.IsTurtle = false;
+                
+                break;
+            
+            case MutationHandler.MutationType.Fish:
+                
+                // Who are you?
+                _mutationSpritesScript.IsNormal = false;
+                _mutationSpritesScript.IsElephant = false;
+                _mutationSpritesScript.IsFish = true;
+                _mutationSpritesScript.IsHawk = false;
+                _mutationSpritesScript.IsRhino = false;
+                _mutationSpritesScript.IsTurtle = false;
+                
+                break;
+            
+            case MutationHandler.MutationType.Hawk:
+                
+                // Who are you?
+                _mutationSpritesScript.IsNormal = false;
+                _mutationSpritesScript.IsElephant = false;
+                _mutationSpritesScript.IsFish = false;
+                _mutationSpritesScript.IsHawk = true;
+                _mutationSpritesScript.IsRhino = false;
+                _mutationSpritesScript.IsTurtle = false;
+                
+                break;
+            
+            case MutationHandler.MutationType.Rhino:
+                
+                // Who are you?
+                _mutationSpritesScript.IsNormal = false;
+                _mutationSpritesScript.IsElephant = false;
+                _mutationSpritesScript.IsFish = false;
+                _mutationSpritesScript.IsHawk = false;
+                _mutationSpritesScript.IsRhino = true;
+                _mutationSpritesScript.IsTurtle = false;
+                
+                break;
+            
+            case MutationHandler.MutationType.Turtle:
+                
+                // Who are you?
+                _mutationSpritesScript.IsNormal = false;
+                _mutationSpritesScript.IsElephant = false;
+                _mutationSpritesScript.IsFish = false;
+                _mutationSpritesScript.IsHawk = false;
+                _mutationSpritesScript.IsRhino = false;
+                _mutationSpritesScript.IsTurtle = true;
+                
+                break;
+            
+            default:
+                return;
+        }
+    }
+    
+    // Calling after by clicking or touching.
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        switch (_mutationType)
+        {
+            case MutationHandler.MutationType.Elephant:
+                
+                // Set default transformation by second click this mutation button.
+                _fishCounter = _hawkCounter = _rhinoCounter = _turtleCounter = 0;
+                _elephantCounter++;
+                if (_elephantCounter > 1)
+                {
+                    _mutationSpritesScript.IsNormal = true;
+                    _mutationSpritesScript.IsElephant = false;
+                    _elephantCounter = 0;
+                }
+                
+                break;
+            
+            case MutationHandler.MutationType.Fish:
+                
+                // Set default transformation by second click this mutation button.
+                _elephantCounter = _hawkCounter = _rhinoCounter = _turtleCounter = 0;
+                _fishCounter++;
+                if (_fishCounter > 1)
+                {
+                    _mutationSpritesScript.IsNormal = true;
+                    _mutationSpritesScript.IsFish = false;
+                    _fishCounter = 0;
+                }
+                
+                break;
+            
+            case MutationHandler.MutationType.Hawk:
+                
+                // Set default transformation by second click this mutation button.
+                _elephantCounter = _fishCounter = _rhinoCounter = _turtleCounter = 0;
+                _hawkCounter++;
+                if (_hawkCounter > 1)
+                {
+                    _mutationSpritesScript.IsNormal = true;
+                    _mutationSpritesScript.IsHawk = false;
+                    _hawkCounter = 0;
+                }
+                
+                break;
+            
+            case MutationHandler.MutationType.Rhino:
+                
+                // Set default transformation by second click this mutation button.
+                _elephantCounter = _fishCounter = _hawkCounter = _turtleCounter = 0;
+                _rhinoCounter++;
+                if (_rhinoCounter > 1)
+                {
+                    _mutationSpritesScript.IsNormal = true;
+                    _mutationSpritesScript.IsRhino = false;
+                    _rhinoCounter = 0;
+                }
+                
+                break;
+            
+            case MutationHandler.MutationType.Turtle:
+                
+                // Set default transformation by second click this mutation button.
+                _elephantCounter = _fishCounter = _hawkCounter = _rhinoCounter = 0;
+                _turtleCounter++;
+                if (_turtleCounter > 1)
+                {
+                    _mutationSpritesScript.IsNormal = true;
+                    _mutationSpritesScript.IsTurtle = false;
+                    _turtleCounter = 0;
+                }
+                
+                break;
+            
+            default:
+                return;
+        }
+    }
+}
---
+++ Darwin/Assets/Scripts/HUD/MutationHandler.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MutationHandler : MonoBehaviour 
+{
+    // Declare variables.
+    public Sprite[] Selected;
+    public Sprite[] DeSelected;
+    private Image _elephant, _fish, _hawk, _rhino, _turtle;
+    
+    /// <summary>
+    /// Set mutation sprite.
+    /// </summary>
+    [Flags]
+    public enum MutationType
+    {
+        Elephant = 1 << 0,
+        Fish = 1 << 1,
+        Hawk = 1 << 2,
+        Rhino = 1 << 3,
+        Turtle = 1 << 4
+    }
+
+    /// <summary>
+    /// Calling before start.
+    /// </summary>
+    private void Awake()
+    {
+        // Get the images.
+        _elephant = transform.GetChild(0).GetComponent<Image>();
+        _fish = transform.GetChild(1).GetComponent<Image>();
+        _hawk = transform.GetChild(2).GetComponent<Image>();
+        _rhino = transform.GetChild(3).GetComponent<Image>();
+        _turtle = transform.GetChild(4).GetComponent<Image>();
+    }
+
+    /// <summary>
+    /// Calling by start.
+    /// </summary>
+    private void Start()
+    {
+        // Declare variables.
+        float yPosition = -25.0f;
+        
+        // Test values.
+        ThreeMutations = new[]
+        {
+            MutationType.Elephant,
+            MutationType.Fish,
+            MutationType.Hawk
+        };
+        
+        // Set default values.
+        IsNormal = true;
+        IsElephant = IsFish = IsHawk = IsRhino = IsTurtle = false;
+
+        // Set mutation icons and activates this.
+        foreach (MutationType currentMutation in ThreeMutations)
+        {
+            if ((currentMutation & MutationType.Elephant) != 0)
+            {
+                transform.GetChild(0).GetComponent<RectTransform>().anchoredPosition =
+                    new Vector3(269.0f, yPosition, 0.0f);
+                transform.GetChild(0).gameObject.SetActive(true);
+            }
+            else if ((currentMutation & MutationType.Fish) != 0)
+            {
+                transform.GetChild(1).GetComponent<RectTransform>().anchoredPosition =
+                    new Vector3(269.0f, yPosition, 0.0f);
+                transform.GetChild(1).gameObject.SetActive(true);
+            }
+            else if ((currentMutation & MutationType.Hawk) != 0)
+            {
+                transform.GetChild(2).GetComponent<RectTransform>().anchoredPosition =
+                    new Vector3(269.0f, yPosition, 0.0f);
+                transform.GetChild(2).gameObject.SetActive(true);
+            }
+            else if ((currentMutation & MutationType.Rhino) != 0)
+            {
+                transform.GetChild(3).GetComponent<RectTransform>().anchoredPosition =
+                    new Vector3(269.0f, yPosition, 0.0f);
+                transform.GetChild(3).gameObject.SetActive(true);
+            }
+            else if ((currentMutation & MutationType.Turtle) != 0)
+            {
+                transform.GetChild(4).GetComponent<RectTransform>().anchoredPosition =
+                    new Vector3(269.0f, yPosition, 0.0f);
+                transform.GetChild(4).gameObject.SetActive(true);
+            }
+
+            // Add offset.
+            yPosition += 50.0f;
+        }
+    }
+
+    /// <summary>
+    /// Körper in einzelne Funktionen aufteilen [DeSelectAll(), SelectElephant(), etc.].**********
+    /// </summary>
+    private void Update()
+    {
+        // By normal, load deselected button sprites.
+        if (IsNormal)
+        {
+            _elephant.sprite = DeSelected[0];
+            _fish.sprite = DeSelected[1];
+            _hawk.sprite = DeSelected[2];
+            _rhino.sprite = DeSelected[3];
+            _turtle.sprite = DeSelected[4];
+        }
+
+        // Who are you?
+        if (IsElephant)
+        {
+            // Switch selected / deselected images.
+            _elephant.sprite = Selected[0];
+            _fish.sprite = DeSelected[1];
+            _hawk.sprite = DeSelected[2];
+            _rhino.sprite = DeSelected[3];
+            _turtle.sprite = DeSelected[4];
+        }
+        else if (IsFish)
+        {
+            // Switch selected / deselected images.
+            _fish.sprite = Selected[1];
+            _elephant.sprite = DeSelected[0];
+            _hawk.sprite = DeSelected[2];
+            _rhino.sprite = DeSelected[3];
+            _turtle.sprite = DeSelected[4];
+        }
+        else if (IsHawk)
+        {
+            // Switch selected / deselected images.
+            _hawk.sprite = Selected[2];
+            _elephant.sprite = DeSelected[0];
+            _fish.sprite = DeSelected[1];
+            _rhino.sprite = DeSelected[3];
+            _turtle.sprite = DeSelected[4];
+        }
+        else if (IsRhino)
+        {
+            // Switch selected / deselected images.
+            _rhino.sprite = Selected[3];
+            _elephant.sprite = DeSelected[0];
+            _fish.sprite = DeSelected[1];
+            _hawk.sprite = DeSelected[2];
+            _turtle.sprite = DeSelected[4];
+        }
+        else if (IsTurtle)
+        {
+            // Switch selected / deselected images.
+            _turtle.sprite = Selected[4];
+            _elephant.sprite = DeSelected[0];
+            _fish.sprite = DeSelected[1];
+            _hawk.sprite = DeSelected[2];
+            _rhino.sprite = DeSelected[3];
+        }
+    }
+
+    // Auto-properties.
+    public MutationType[] ThreeMutations { private get; set; }
+    public bool IsNormal { get; set; }
+    public bool IsElephant { get; set; }
+    public bool IsFish { get; set; }
+    public bool IsHawk { get; set; }
+    public bool IsRhino { get; set; }
+    public bool IsTurtle { get; set; }
+}
